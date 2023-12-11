32a9b4840e6aaf6eee849657e9ab80c9e21b8b9b
pratimabudhathoki <devendra13.dc@gmail.com>
2023-12-11 16:41:06 +0545

    added style

--- src/App.js
+++ src/App.js
@@ -132,68 +132,99 @@ const App = () => {
     if (encryptedFile && decryptPassword) {
       try {
         const decryptedData = await decryptFile(encryptedFile, decryptPassword);
-        setDecryptedFile(new Blob([decryptedData], { type: "application/pdf" }));
+        setDecryptedFile(
+          new Blob([decryptedData], { type: "application/pdf" })
+        );
       } catch (error) {
         console.error("Decryption failed:", error);
         alert("Decryption failed. Incorrect password or corrupted file.");
       }
     } else {
-      alert("Please provide an encrypted file and enter the decryption password.");
+      alert(
+        "Please provide an encrypted file and enter the decryption password."
+      );
     }
   };
 
   return (
-    <div>
-      <h2>File Encryptor and Decryptor</h2>
-      <input type="file" onChange={handleFileChange} />
+    <div className="w-screen flex flex-col items-center gap-[10px] mt-[50px]">
+      <h2 className="text-xl font-bold underline underline-offset-8">
+        File Encryptor and Decryptor
+      </h2>
+      <input type="file" onChange={handleFileChange} className="mt-[30px]" />
       <br />
       <label>
         Enter password for encryption:
-        <input type="password" placeholder="Enter password" onChange={handlePasswordChange} />
+        <input
+          type="password"
+          placeholder="Enter password"
+          onChange={handlePasswordChange}
+          className="p-[6px] rounded-[4px] outline-none border border-slate-300 ml-[10px] "
+        />
       </label>
       <br />
-      <button onClick={handleEncrypt}>Encrypt File</button>
+      <button
+        onClick={handleEncrypt}
+        className="bg-pink-600 px-[20px] py-[8px] text-white rounded-[10px] "
+      >
+        Encrypt File
+      </button>
       <br />
       <label>
         Enter password for decryption:
-        <input type="password" placeholder="Enter decryption password" onChange={handleDecryptPasswordChange} />
+        <input
+          type="password"
+          placeholder="Enter decryption password"
+          onChange={handleDecryptPasswordChange}
+          className="p-[6px] rounded-[4px] outline-none border border-slate-300 ml-[10px] "
+        />
       </label>
       <br />
-      <button onClick={handleDecrypt}>Decrypt File</button>
+      <button
+        onClick={handleDecrypt}
+        className="bg-blue-600 px-[20px] py-[8px] text-white rounded-[10px] "
+      >
+        Decrypt File
+      </button>
       <br />
-      {encryptedFile && (
-        <div>
-          <h3>Encrypted File:</h3>
-          <a
-            href={URL.createObjectURL(new Blob([encryptedFile]))}
-            download="encrypted_file"
-          >
-            Download Encrypted File
-          </a>
-        </div>
-      )}
-      {decryptedFile && (
-        <div>
-          <h3>Decrypted File:</h3>
-          <iframe
-            title="Decrypted File"
-            src={URL.createObjectURL(decryptedFile)}
-            width="100%"
-            height="600px"
-          ></iframe>
-        </div>
-      )}
-      {decryptedFile && (
-        <div>
-          <h3>Decrypted File:</h3>
-          <a
-            href={URL.createObjectURL(decryptedFile)}
-            download="decrypted_file"
-          >
-            Download Decrypted File
-          </a>
-        </div>
-      )}
+
+      <div className="w-screen min-h-[30vh] bg-pink-300 flex items-center justify-evenly p-[20px]">
+        {encryptedFile && (
+          <div className="flex flex-col gap-[10px]">
+            <h3>Encrypted File:</h3>
+            <a
+              href={URL.createObjectURL(new Blob([encryptedFile]))}
+              download="encrypted_file"
+              className="bg-red-500 text-white p-[7px]"
+            >
+              Download Encrypted File
+            </a>
+          </div>
+        )}
+        {decryptedFile && (
+          <div className="flex flex-col gap-[10px]">
+            <h3>Decrypted File:</h3>
+            <iframe
+              title="Decrypted File"
+              src={URL.createObjectURL(decryptedFile)}
+              width="500px"
+              height="400px"
+            ></iframe>
+          </div>
+        )}
+        {decryptedFile && (
+          <div className="flex flex-col gap-[10px]">
+            <h3>Decrypted File:</h3>
+            <a
+              href={URL.createObjectURL(decryptedFile)}
+              download="decrypted_file"
+              className="bg-blue-500 text-white p-[7px]"
+            >
+              Download Decrypted File
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
